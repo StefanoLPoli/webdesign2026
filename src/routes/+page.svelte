5d4b86fa84904c900fd570e86ad46c6e24623d82
Stefano Lorini <stefano.lorini@mail.polimi.it>
2026-03-25 17:28:37 +0100

    Add counter and derived doubled value
    Update page script and UI: change debug log, introduce reactive state variables `count` and `doubled`, add a button to increment `count` and display both the current count and its doubled value. Small UI additions to the SvelteKit page to demonstrate reactive state.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <script>
-    console.log('Ziopppera!');
+    console.log('Logsss!');
+    let count = $state(0);
+    let doubled = $derived(count * 2);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={()=> count++}>
+    Click: {count}
+</button>
+
+<span>Doubled: {doubled}</span>
 
 <style>
 	h1 {
