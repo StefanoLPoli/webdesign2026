37924198ecccec6e52e7fa16ea92e28ad60ecab9
Stefano Lorini <stefano.lorini@mail.polimi.it>
2026-04-08 17:51:41 +0200

    todos

---
+++ src/lib/components/TodoList.svelte
@@ -0,0 +1,18 @@
+<script>
+    let todos = $state([
+        { title: "Assignment 1", done: true },
+        { title: "Assignment 2", done: false },
+        { title: "Assignment 3", done: false }
+    ]);
+</script>
+
+<h3>My Todo List</h3>
+
+<ul>
+    {#each todos as todo}
+        <li>
+            <input type="checkbox" bind:checked={todo.done}>
+            <span class:done={todo.done}>{todo.title}</span>
+        </li>
+    {/each}
+</ul>
